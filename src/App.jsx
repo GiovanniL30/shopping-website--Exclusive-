0049827a99515a0edd7c35d1d716f91e1fb1341d
Giovanni Leo <2232254@slu.edu.ph>
2024-06-23 10:17:01 +0800

    implemented a loading animation when loading products

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MainLayout from './layout/MainLayout'
 import {
   Route,
@@ -9,10 +9,27 @@ import {
 import { About, Contact, Cart, Home, Signup } from './pages'
 import { loader as productsLoader } from './pages/Home'
 
+const HomeLoading = () => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetchProducts() {
+      await productsLoader()
+      setLoading(false)
+    }
+
+    fetchProducts()
+  }, [])
+
+  if (loading) return <h1>Loading Products</h1>
+
+  return <Home />
+}
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
-      <Route loader={productsLoader} index element={<Home />} />
+      <Route loader={productsLoader} index element={<HomeLoading />} />
       <Route path='contact' element={<Contact />} />
       <Route path='cart' element={<Cart />} />
       <Route path='signup' element={<Signup />} />
