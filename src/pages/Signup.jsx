5500268108f5a8b7cf4996aa1d3b7990eaecf778
Giovanni Leo <2232254@slu.edu.ph>
2024-06-25 21:00:20 +0800

    did the initial implementation of the sign up page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import image from '../../public/assets/Side Image.png'
 
 const Signup = () => {
-  return <div>Signup</div>
+  return (
+    <div className='relative lg:h-[900px]'>
+      <div className='padding'>
+        <div className='max-container flex lg:justify-end'>
+          <div className='z-20 w-full flex flex-col lg:w-1/2'>
+            <h1 className='text-4xl font-semibold tracking-wide'>
+              Create an account
+            </h1>
+            <p>Enter your details below</p>
+            <div className='flex flex-col'>
+              <div className='relative w-full'>
+                <label
+                  htmlFor='name'
+                  className='absolute bottom-2 text-slate-300'
+                >
+                  Name
+                </label>
+                <input
+                  className='w-full border-b-[1px] border-b-slate-300 outline-none pb-2'
+                  type='text'
+                  id='name'
+                />
+              </div>
+            </div>
+
+            <div>
+              <button>Create Account</button>
+              <button>Sign up with Google</button>
+            </div>
+
+            <p>
+              Already have account? <NavLink>Log in</NavLink>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='hidden h-[700px] lg:block  lg:w-[900px]  absolute left-0 top-10'>
+        <img
+          src={image}
+          className='w-full object-fill h-full'
+          alt='Side Image'
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Signup
